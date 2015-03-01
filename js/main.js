733069b499fac936c94478a72898d6dc4d229d8c
Ilnur Khalilov <xomyaq@gmail.com>
2015-03-02 01:37:25 +0300

    Fortune cookie

--- js/main.js
+++ js/main.js
@@ -1,11 +1,74 @@
-;(function (window, document, $, SP, Promise) {
+/* global SAILPLAY, SP, USER_EMAIL */
+;(function (window, document, $, sailplay, Promise, userEmail) {
+  /**
+   * Today
+   * @const
+   */
+  var TODAY = (function (date) { return [date.getDate(), date.getMonth(), date.getFullYear()].join(''); }(new Date()));
+
+  var FORTUNE_DATA = {
+    '122015': 'У вас на этой неделе все будет хорошо! ;)',
+    '222015': '2 марта у вас все будет хорошо! ;)',
+    '322015': '3 марта у вас все будет хорошо! ;)',
+    '422015': '4 марта у вас все будет хорошо! ;)',
+    '522015': '5 марта у вас все будет хорошо! ;)'
+  };
+
+  /**
+   * Cookie manager
+   * @type {{write, read, erase}}
+   */
+  var cookie = (function () {
+    var _write = function (name, value, days) {
+      var expires = ''
+        , date;
+
+      if (days) {
+        date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+
+      document.cookie = name + "=" + value + expires + "; path=/";
+    };
+
+    var _read = function (name) {
+      var nameEQ = name + "="
+        , cookiesArray = document.cookie.split(';')
+        , i, cookie;
+
+      for (i = 0; i < cookiesArray.length; i++) {
+        cookie = cookiesArray[i];
+
+        while (cookie.charAt(0) == ' ') {
+          cookie = cookie.substring(1, cookie.length);
+        }
+
+        if (cookie.indexOf(nameEQ) == 0) {
+          return cookie.substring(nameEQ.length, cookie.length);
+        }
+      }
+
+      return null;
+    };
+
+    var _erase = function (name) {
+      _write(name, "", -1);
+    };
+
+    return {
+      write: _write,
+      read: _read,
+      erase: _erase
+    };
+  }());
 
   /**
    * SailPlay API
    *
-   * @type {{init, login, userInfo, userHistory}}
+   * @type {{init, login, userInfo, userHistory, setTags, getTags}}
    */
-  var api = (function (SP, Promise) {
+  var api = (function () {
     var partnerId, authHash, instance, user;
 
     return {
@@ -13,10 +76,10 @@
         partnerId = id;
 
         return instance ? instance : new Promise(function (resolve, reject) {
-          SP.send('init', { partner_id: id });
+          sailplay.send('init', { partner_id: id });
 
-          SP.on('init.success', resolve);
-          SP.on('init.error', reject);
+          sailplay.on('init.success', resolve);
+          sailplay.on('init.error', reject);
         });
       },
 
@@ -24,20 +87,20 @@
         authHash = hash;
 
         return user ? user : new Promise(function (resolve, reject) {
-          SP.send('login', hash);
+          sailplay.send('login', hash);
 
-          SP.on('login.success', resolve);
-          SP.on('login.error', reject);
+          sailplay.on('login.success', resolve);
+          sailplay.on('login.error', reject);
         });
       },
 
       userInfo: function () {
         return instance.then(user).then(function () {
           return new Promise(function (resolve, reject) {
-            SP.send('load.user.info');
+            sailplay.send('load.user.info');
 
-            SP.on('load.user.info.success', resolve);
-            SP.on('load.user.info.error', reject);
+            sailplay.on('load.user.info.success', resolve);
+            sailplay.on('load.user.info.error', reject);
           });
         })
       },
@@ -45,19 +108,91 @@
       userHistory: function () {
         return instance.then(user).then(function () {
           return new Promise(function (resolve, reject) {
-            SP.send('load.user.history');
+            sailplay.send('load.user.history');
 
-            SP.on('load.user.history.success', resolve);
-            SP.on('load.user.history.error', reject);
+            sailplay.on('load.user.history.success', resolve);
+            sailplay.on('load.user.history.error', reject);
           });
         });
+      },
+
+      setTags: function (tags) {
+        window.SP.tags({
+          action: 'add',
+          tags: tags,
+          email: userEmail
+        })
+      },
+
+      getTags: function (cb) {
+        window.SP.tags({
+          action: 'list',
+          email: userEmail,
+          callback: cb
+        })
       }
     }
-  }(SP, Promise));
+  }());
+
+  var quiz = (function () {
+    var data = {
+      '122015': {
+        '1': {
+          pic: 'img/slide4/image-2.png',
+          question: 'ЧТО ИЗОБРАЖЕНО НА ФОТО?',
+          variants: ['Броги', 'Сникерсы', 'Челси', 'Челси'],
+          answer: 'Броги'
+        },
+        '2': {
+          pic: 'img/slide4/image-2.png',
+          question: 'ЧТО ИЗОБРАЖЕНО НА ФОТО?',
+          variants: ['Броги', 'Сникерсы', 'Челси', 'Челси'],
+          answer: 'Сникерсы'
+        },
+        '3': {
+          pic: 'img/slide4/image-2.png',
+          question: 'ЧТО ИЗОБРАЖЕНО НА ФОТО?',
+          variants: ['Броги', 'Сникерсы', 'Челси', 'Челси'],
+          answer: 'Челси'
+        },
+        '4': {
+          pic: 'img/slide4/image-2.png',
+          question: 'ЧТО ИЗОБРАЖЕНО НА ФОТО?',
+          variants: ['Броги', 'Сникерсы', 'Челси', 'Челси'],
+          answer: 'Дерби'
+        }
+      }
+    };
+
+    return {
+      init: function () {
+        // Init
+      }
+    }
+  }());
+
+  var fortune = (function () {
+    return {
+      show: function () {
+        var $parent = $('[data-root-cookie]');
+
+        $parent.addClass('hide-cookie-all');
+
+        window.setTimeout(function () {
+          $parent.addClass('show-cookie');
+        }, 600);
+      },
+
+      read: function () {
+        api.setTags(['Прочитал предсказание']);
+        cookie.write('fortune', 'read', 1);
+      }
+    };
+  }());
 
   var $body = $('body');
 
-// common slider
+  // common slider
   $(document.body).addClass('scroll-enabled');
 
   var slideAnchors = [
@@ -68,17 +203,17 @@
       'slide-5',
       'slide-6',
       'slide-7'
-    ],
-    bottomArrowTexts = [
+    ]
+    , bottomArrowTexts = [
       'Призы',
       'Зарабатывайте баллы',
       'Предсказания дня',
       'Викторина',
       'Наши лидеры',
       'Наши лидеры',
-      '',
-    ],
-    bodyClasses = [
+      ''
+    ]
+    , bodyClasses = [
       'body-slide-1',
       'body-slide-2',
       'body-slide-3',
@@ -128,25 +263,25 @@
     });
 
 
-// for 1 slide
+  // for 1 slide
   var $welcome = $('[data-root-welcome]');
 
   $body
     .on('click', '[data-reg-next]', function(e){
       $welcome
-        .addClass('show-slide-1-complete')
+        .addClass('show-slide-1-complete');
 
       e.preventDefault();
     })
     .on('click', '[data-btn-history]', function(e){
       $welcome
-        .addClass('show-slide-1-history')
+        .addClass('show-slide-1-history');
 
       e.preventDefault();
     })
     .on('click', '[data-btn-close-history]', function(e){
       $welcome
-        .removeClass('show-slide-1-history')
+        .removeClass('show-slide-1-history');
 
       e.preventDefault();
     })
@@ -208,8 +343,7 @@
   });
 
 
-
-//for 2 slide
+  // for 2 slide
   var $rootGift = $('[data-gift-root]')
     , timer;
 
@@ -238,31 +372,29 @@
     });
 
 
-//for 3 slide
-  $body
-    .on('click', '[data-cookie]', function(e){
-      var $parent = $('[data-root-cookie]');
+  // for 3 slide
+  $('[data-cookie-text]').text(FORTUNE_DATA[TODAY]);
 
-      $parent.addClass('hide-cookie-all');
+  if (cookie.read('fortune') !== null) {
+    fortune.show();
+  }
 
-      setTimeout( function(){
-        $parent.addClass('show-cookie');
-      }, 600);
+  $body.on('click', '[data-cookie]', function (e) {
+    e.preventDefault();
 
-      e.preventDefault();
-    });
+    fortune.show();
+    fortune.read();
+  });
 
-// for 4 slide
-  var step = 1,
-    $testRoot = $('[data-test-root]');
+  // for 4 slide
+  var step = 1
+    , $testRoot = $('[data-test-root]');
 
   $body
     .on('click', '[data-test-start], [data-test-btn]', function(e){
       $testRoot
         .removeClass('show-test-' + step)
         .removeClass('show-buttons');
-
-
 
       step += 1;
       $testRoot.addClass('show-test-' + step);
@@ -279,4 +411,4 @@
   //  console.log(res);
   //});
 
-}(window, document, jQuery, SAILPLAY, vow.Promise));
+}(window, document, window.jQuery, window.SAILPLAY, vow.Promise, window.USER_EMAIL));
